1129c4dc7221a31d7712c4436a0e08d788f84510
redfxfcgvhjbknlm <117573162+redfxfcgvhjbknlm@users.noreply.github.com>
2026-03-02 11:16:22 +0530

    Update montgomery_cios_flat.v

--- montgomery_cios_flat.v
+++ montgomery_cios_flat.v
@@ -1,3 +1,13 @@
+// Welcome to JDoodle!
+//
+// You can execute code here in 88 languages. Right now you’re in the Verilog IDE. 
+//
+//  1. Click the orange Execute button ️▶ to execute the sample code below and see how it works.
+//  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+//  3. Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+//
+// Want to change languages? Try the search bar up the top.
+
 module montgomery_cios_flat #(
     parameter WORD_WIDTH = 32,
     parameter NWORDS     = 64
@@ -25,13 +35,14 @@ module montgomery_cios_flat #(
     reg [WORD_WIDTH:0] carry;
     reg [WORD_WIDTH:0] sum;
 
+    reg [$clog2(NWORDS):0] done_idx;
     reg [$clog2(NWORDS):0] i;
     reg [$clog2(NWORDS):0] j;
     reg [$clog2(NWORDS):0] shift_cnt;
     reg [$clog2(NWORDS):0] cmp_cnt;
     reg [$clog2(NWORDS):0] sub_cnt;
 
-    reg ge_flag;
+    reg ge_flag,cmp_done;
 
     localparam IDLE  = 0,
                MUL1  = 1,
@@ -228,17 +239,19 @@ end
             // --------------------------------
             //DONE
             // --------------------------------
-            DONE:
-            begin
-                if (!ge_flag)
+            DONE: 
                 begin
-                    result_flat <= {T[NWORDS-1:0]};
+                    if (!ge_flag)
+                    begin
+                        // Use generate block to create assignments for each word
+                        for (done_idx = 0; done_idx < NWORDS; done_idx = done_idx + 1) begin : done_assignments
+                            result_flat[done_idx*WORD_WIDTH +: WORD_WIDTH] <= T[done_idx];
+                        end
+                    end
+                
+                    done  <= 1;
+                    state <= IDLE;
                 end
-
-                done  <= 1;
-                state <= IDLE;
-            end
-
             endcase
         end
     end
